e0ca2f376e1d1c2b39c202e8909da14baede2975
tomvieilledent <tomvieilledent@gmail.com>
2026-02-24 15:44:04 +0100

    Refactor model structure: reintroduce BaseModel and implement Amenity, Place, Review, and Users classes with validation methods

---
+++ part2/hbnb/app/models/place.py
@@ -0,0 +1,103 @@
+from base_model import BaseModel
+from user import Users
+
+from user import Users
+
+
+class Place(BaseModel):
+    def __init__(self, title, description, price, latitude, longitude, owner):
+        super().__init__()
+        self.title = self.title_verif(title)
+        self.description = self.description_verif(description)
+        self.price = self.price_verif(price)
+        self.latitude = self.latitude_verif(latitude)
+        self.longitude = self.longitude_verif(longitude)
+        self.owner = self.owner_verif(owner)
+        self.reviews = []  # List to store related reviews
+        self.amenities = []  # List to store related amenities
+
+    def title_verif(self, title):
+        if not isinstance(title, str):
+            raise TypeError("Title must be a string.")
+        if len(title) > 100:
+            raise ValueError("Title cannot exceed 100 characters.")
+        return title
+
+    def description_verif(self, description):
+        if description:
+            if not isinstance(description, str):
+                raise TypeError("Description must be a string.")
+            return description
+
+    def price_verif(self, price):
+        if not isinstance(price, float):
+            raise TypeError("Price must be a float.")
+        if price < 0:
+            raise ValueError("Price must be positive.")
+        return price
+
+    def latitude_verif(self, latitude):
+        if not isinstance(latitude, float):
+            raise TypeError("Latitude must be a float.")
+        if not (-90 <= latitude <= 90):
+            raise ValueError("Latitude must be in range -90 : 90.")
+        return latitude
+
+    def longitude_verif(self, longitude):
+        if not isinstance(longitude, float):
+            raise TypeError("Longitude must be a float.")
+        if not (-180 <= longitude <= 180):
+            raise ValueError("Longitude must be in range -180 : 180.")
+        return longitude
+
+    def owner_verif(self, owner):
+        if not isinstance(owner, Users):
+            raise TypeError("Owner must be an user.")
+        return owner
+
+    def title_verif(self, title):
+        if not isinstance(title, str):
+            raise TypeError("Title must be a string.")
+        if len(title) > 100:
+            raise ValueError("Title cannot exceed 100 characters.")
+        return title
+
+    def description_verif(self, description):
+        if description:
+            if not isinstance(description, str):
+                raise TypeError("Description must be a string.")
+            return description
+
+    def price_verif(self, price):
+        if not isinstance(price, float):
+            raise TypeError("Price must be a float.")
+        if price < 0:
+            raise ValueError("Price must be positive.")
+        return price
+
+    def latitude_verif(self, latitude):
+        if not isinstance(latitude, float):
+            raise TypeError("Latitude must be a float.")
+        if not (-90 <= latitude <= 90):
+            raise ValueError("Latitude must be in range -90 : 90.")
+        return latitude
+
+    def longitude_verif(self, longitude):
+        if not isinstance(longitude, float):
+            raise TypeError("Longitude must be a float.")
+        if not (-180 <= longitude <= 180):
+            raise ValueError("Longitude must be in range -180 : 180.")
+        return longitude
+
+    def owner_verif(self, owner):
+        if not isinstance(owner, Users):
+            raise TypeError("Owner must be an user.")
+        return owner
+
+    def add_review(self, review):
+        """Add a review to the place."""
+        self.reviews.append(review)
+
+    def add_amenity(self, amenity):
+        """Add an amenity to the place."""
+        self.amenities.append(amenity)
---
+++ part2/hbnb/app/models/user.py
@@ -0,0 +1,36 @@
+from base_model import BaseModel
+from email_validator import validate_email, EmailNotValidError
+
+class Users(BaseModel):
+    def __init__(self, first_name, last_name, email, password, is_admin=False):
+        super().__init__()
+        self.first_name = self.first_name_verif(first_name)
+        self.last_name = self.last_name_verif(last_name)
+        self.email = self.email_verif(email)
+        self.password = password
+        self.is_admin = is_admin
+
+    def first_name_verif(self, first_name):
+        if not isinstance(first_name, str):
+            raise TypeError("First name must be a string. ")
+        if len(first_name) > 50:
+            raise ValueError("First name cannot exceed 50 characters.")
+        return first_name
+
+    def last_name_verif(self, last_name):
+        if not isinstance(last_name, str):
+            raise TypeError("Last name must be a string.")
+        if len(last_name) > 50:
+            raise ValueError("Last name cannot exceed 50 characters.")
+        return last_name
+
+    def email_verif(self, email):
+        if not isinstance(email, str):
+            raise TypeError("Email must be a string.")
+        try:
+            validate_email(email)
+        except EmailNotValidError:
+            raise TypeError("Invalid email address format.")
+        # *** A faire, vérifier que dans la data base l'adresse mail n'existe pas *** #
+        return email
+    
