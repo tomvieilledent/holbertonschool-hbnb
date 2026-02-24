f8e6acfcd827409b409415ef70acf6ac0e9c9661
Florian <12160@holbertonstudents.com>
2026-02-24 14:42:21 +0100

    Refactor validation methods in Amenity, Place, Review, and Users classes for consistency and clarity

--- part2/hbnb/app/api/v1/amenities.py
+++ part2/hbnb/app/api/v1/amenities.py
@@ -2,6 +2,7 @@
 
 from base_model import BaseModel
 
+
 class Amenity(BaseModel):
     def __init__(self, name):
         super().__init__()
--- part2/hbnb/app/api/v1/places.py
+++ part2/hbnb/app/api/v1/places.py
@@ -3,6 +3,7 @@ from users import Users
 
 from users import Users
 
+
 class Place(BaseModel):
     def __init__(self, title, description, price, latitude, longitude, owner):
         super().__init__()
@@ -14,7 +15,7 @@ class Place(BaseModel):
         self.owner = self.owner_verif(owner)
         self.reviews = []  # List to store related reviews
         self.amenities = []  # List to store related amenities
-    
+
     def title_verif(self, title):
         if not isinstance(title, str):
             raise TypeError("Title must be a string.")
@@ -27,28 +28,32 @@ class Place(BaseModel):
             if not isinstance(description, str):
                 raise TypeError("Description must be a string.")
             return description
-        
+
     def price_verif(self, price):
         if not isinstance(price, float):
             raise TypeError("Price must be a float.")
         if price < 0:
             raise ValueError("Price must be positive.")
-    
+        return price
+
     def latitude_verif(self, latitude):
         if not isinstance(latitude, float):
             raise TypeError("Latitude must be a float.")
         if not (-90 <= latitude <= 90):
             raise ValueError("Latitude must be in range -90 : 90.")
-        
+        return latitude
+
     def longitude_verif(self, longitude):
         if not isinstance(longitude, float):
             raise TypeError("Longitude must be a float.")
         if not (-180 <= longitude <= 180):
             raise ValueError("Longitude must be in range -180 : 180.")
-        
+        return longitude
+
     def owner_verif(self, owner):
         if not isinstance(owner, Users):
             raise TypeError("Owner must be an user.")
+        return owner
 
     def title_verif(self, title):
         if not isinstance(title, str):
