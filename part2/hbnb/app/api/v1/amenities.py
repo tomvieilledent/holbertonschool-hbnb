9ab74c52479236290664556cd10bd03af11af907
tomvieilledent <tomvieilledent@gmail.com>
2026-02-24 11:24:28 +0100

    Implement name length verification in Amenity class constructor

--- part2/hbnb/app/api/v1/amenities.py
+++ part2/hbnb/app/api/v1/amenities.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
+from BaseModel import BaseModel
+
 class Amenity(BaseModel):
     def __init__(self, name):
         super().__init__()
-        self.name = name
+        self.name = self.verify_len(name)
+
+    def verify_len(name):
+        if len(name) < 50:
+            return name
+        else:
+            raise ValueError("Amenity name cannot exceed 50 characters")
