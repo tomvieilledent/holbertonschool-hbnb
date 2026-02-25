8e9b91f4ab99c1f68c169aabfdab75b6ba0c60f1
tomvieilledent <tomvieilledent@gmail.com>
2026-02-25 13:04:05 +0100

    Add user retrieval and update methods in HBnBFacade class

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -10,7 +10,7 @@ class HBnBFacade:
         self.place_repo = InMemoryRepository()
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
-
+#Users
     def create_user(self, user_data):
         user = User(**user_data)
         self.user_repo.add(user)
@@ -21,8 +21,24 @@ class HBnBFacade:
 
     def get_user_by_email(self, email):
         return self.user_repo.get_by_attribute('email', email)
+    
+    def get_all_users(self):
+        return self.user_repo.get_all()
 
+    def update_user(self, user_id, user_data):
+        user = self.get_user(user_id)
+        if not user:
+            raise ValueError("User not found")
+        self.user_repo.update(user_id, user_data)
+        return user
+        
+        
+#Places
     # Placeholder method for fetching a place by ID
     def get_place(self, place_id):
         # Logic will be implemented in later tasks
         pass
+
+#Amenities
+
+#Reviews
