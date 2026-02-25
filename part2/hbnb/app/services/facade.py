3205dc16f97c0b1fc9966c298531496773a2167c
Florian <12160@holbertonstudents.com>
2026-02-25 10:24:44 +0100

    Implement user registration endpoint with email uniqueness check and user creation logic

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from app.persistence.repository import InMemoryRepository
+from app.models.user import User
 
 
 class HBnBFacade:
@@ -10,10 +11,16 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    # Placeholder method for creating a user
     def create_user(self, user_data):
-        # Logic will be implemented in later tasks
-        pass
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
+
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
 
     # Placeholder method for fetching a place by ID
     def get_place(self, place_id):
