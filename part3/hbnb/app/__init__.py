ef351180b16592f7400bdefdb7a54d3c1caad8df
tomvieilledent <tomvieilledent@gmail.com>
2026-03-10 11:28:21 +0100

    Implement Flask-SQLAlchemy integration and update configuration settings

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -2,10 +2,15 @@
 
 from flask import Flask
 from flask_restx import Api
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 
-def create_app():
+def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
+    app.config.from_object(config_class)
+    db.init_app(app)
+
     api = Api(app, version='1.0', title='HBnB API',
               description='HBnB Application API', doc='/')
 
@@ -18,5 +23,8 @@ def create_app():
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+
+    with app.app_context():
+        db.create_all()
 
     return app
