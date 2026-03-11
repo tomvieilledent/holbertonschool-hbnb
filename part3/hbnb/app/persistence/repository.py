8f8b257e8963cec612fc3e65e9a6a2127d8c7292
tomvieilledent <tomvieilledent@gmail.com>
2026-03-11 12:37:25 +0100

    Refactor repository and facade to improve code clarity and consistency

--- part3/hbnb/app/persistence/repository.py
+++ part3/hbnb/app/persistence/repository.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 from abc import ABC, abstractmethod
-from app import db  # Assuming you have set up SQLAlchemy in your Flask app
-from app.models import User, Place, Review, Amenity  # Import your models
+from app import db
 
 
 
