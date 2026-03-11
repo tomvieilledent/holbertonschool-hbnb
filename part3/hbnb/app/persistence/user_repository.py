c22a4b4dafc30d06facc366324fe311063a7408b
tomvieilledent <tomvieilledent@gmail.com>
2026-03-11 15:46:50 +0100

    Add user repository and enhance user model with password hashing and timestamps

---
+++ part3/hbnb/app/persistence/user_repository.py
@@ -0,0 +1,9 @@
+from repository import SQLAlchemyRepository
+from models.user import User
+
+class UserRepository(SQLAlchemyRepository):
+    def __init__(self):
+        super().__init__(User)
+
+    def get_user_by_email(self, email):
+        return self.model.query.filter_by(email=email).first()
