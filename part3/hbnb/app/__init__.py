7e89fa80ea21256122645b8451222806f8f931ee
Florian <12160@holbertonstudents.com>
2026-03-12 14:13:25 +0100

    Implement JWT authentication with login endpoint and update app initialization

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -4,8 +4,13 @@ from flask import Flask
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
+jwt = JWTManager()
+
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
@@ -20,12 +25,17 @@ def create_app(config_class="config.DevelopmentConfig"):
     from app.api.v1.amenities import api as amenities_ns
     from app.api.v1.places import api as places_ns
     from app.api.v1.reviews import api as reviews_ns
+    from app.api.v1.auth import api as auth_ns
+    
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
 
     with app.app_context():
         db.create_all()
 
+    bcrypt.init_app(app)
+    jwt.init_app(app)
     return app
